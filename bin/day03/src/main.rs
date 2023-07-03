25840d339e9b17e5d673e7a881eda75858f63fb6
David Antliff <david@pitchblende.co.nz>
2023-07-04 10:15:03 +1200

    Refactor to use an Item abstraction, inspired by https://fasterthanli.me/series/advent-of-code-2022/part-3.

--- bin/day03/src/main.rs
+++ bin/day03/src/main.rs
@@ -4,13 +4,53 @@ use std::collections::HashSet;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
+
+    let _a = Item::try_from('a');
+
     select_and_solve("inputs/day03.1", part1, "inputs/day03.2", part2)?;
     Ok(())
 }
 
+trait Priority {
+    fn priority(&self) -> i32;
+}
+
+// Abstraction of an Item, hidden in a mod to make the constructor private (i.e. unusable)
+// https://fasterthanli.me/series/advent-of-code-2022/part-3
+mod item {
+    #[repr(transparent)] // not needed?
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+    pub struct Item(char);
+
+    impl TryFrom<char> for Item {
+        type Error = color_eyre::eyre::Report;
+
+        fn try_from(value: char) -> Result<Self, Self::Error> {
+            match value {
+                'a'..='z' | 'A'..='Z' => Ok(Item(value)),
+                _ => Err(color_eyre::eyre::eyre!("{} is not a valid item", value)),
+            }
+        }
+    }
+
+    use super::Priority;
+
+    impl Priority for Item {
+        fn priority(&self) -> i32 {
+            match self {
+                Item('a'..='z') => 1 + (self.0 as i32 - 'a' as i32) as i32,
+                Item('A'..='Z') => 27 + (self.0 as i32 - 'A' as i32) as i32,
+                _ => unreachable!(),
+            }
+        }
+    }
+}
+
+use item::Item;
+
 // A rucksack is represented as two hashsets of items
 #[derive(Debug, PartialEq)]
-struct Rucksack(HashSet<char>, HashSet<char>);
+struct Rucksack(HashSet<Item>, HashSet<Item>);
 
 impl TryFrom<&str> for Rucksack {
     type Error = color_eyre::Report;
@@ -20,18 +60,22 @@ impl TryFrom<&str> for Rucksack {
         match len % 2 {
             0 => {
                 let (a, b) = value.split_at(len / 2);
-                Ok(Rucksack(a.chars().collect(), b.chars().collect()))
+                let first_items = a
+                    .chars()
+                    .map(Item::try_from)
+                    .collect::<Result<HashSet<Item>>>()?;
+                let second_items = b
+                    .chars()
+                    .map(Item::try_from)
+                    .collect::<Result<HashSet<Item>>>()?;
+                Ok(Rucksack(first_items, second_items))
             }
             _ => Err(eyre!("not even")),
         }
     }
 }
 
-trait Priority {
-    fn priority(&self) -> i32;
-}
-
-impl Priority for HashSet<char> {
+impl Priority for HashSet<Item> {
     fn priority(&self) -> i32 {
         let mut sum = 0;
         for c in self {
@@ -81,9 +125,8 @@ fn part1(input: Vec<String>) -> Result<String> {
     // for each item in ruckscks, get intersection and calculate priority
     let mut priority_sum = 0;
     for rucksack in rucksacks {
-        //let in_both: HashSet<_> = rucksack.0.intersection(&rucksack.1);
         let in_both: HashSet<_> = rucksack.0.intersection(&rucksack.1).cloned().collect();
-        println!("{:?}", in_both);
+        //dbg!("{:?}", &in_both);
 
         priority_sum += in_both.priority();
     }
@@ -111,9 +154,9 @@ fn part2(input: Vec<String>) -> Result<String> {
     for group in groups {
         assert_eq!(group.len(), 3);
 
-        let unions: Vec<HashSet<char>> = group
+        let unions: Vec<HashSet<Item>> = group
             .iter()
-            .map(|r| r.0.union(&r.1).cloned().collect::<HashSet<char>>())
+            .map(|r| r.0.union(&r.1).cloned().collect::<HashSet<Item>>())
             .collect();
 
         assert_eq!(unions.len(), 3);
@@ -175,8 +218,22 @@ CrZsJsPPZsGzwwsLwLmpwMDw"
     fn test_rucksack_tryfrom() {
         let b = Rucksack::try_from("abcDEF");
         let b = b.expect("not error");
-        assert_eq!(b.0, "abc".chars().collect());
-        assert_eq!(b.1, "DEF".chars().collect());
+        assert_eq!(
+            b.0,
+            HashSet::from([
+                Item::try_from('a').unwrap(),
+                Item::try_from('b').unwrap(),
+                Item::try_from('c').unwrap()
+            ])
+        );
+        assert_eq!(
+            b.1,
+            HashSet::from([
+                Item::try_from('D').unwrap(),
+                Item::try_from('E').unwrap(),
+                Item::try_from('F').unwrap()
+            ])
+        );
     }
 
     #[test]
@@ -189,8 +246,16 @@ CrZsJsPPZsGzwwsLwLmpwMDw"
 
     #[test]
     fn test_intersection_priority() {
-        let a: HashSet<_> = "abc".chars().collect();
-        let b: HashSet<_> = "bcd".chars().collect();
+        let a: HashSet<_> = "abc"
+            .chars()
+            .map(Item::try_from)
+            .collect::<Result<HashSet<Item>>>()
+            .unwrap();
+        let b: HashSet<_> = "bcd"
+            .chars()
+            .map(Item::try_from)
+            .collect::<Result<HashSet<Item>>>()
+            .unwrap();
 
         let i: HashSet<_> = a.intersection(&b).cloned().collect();
         assert_eq!(i.priority(), 'b'.priority() + 'c'.priority());
