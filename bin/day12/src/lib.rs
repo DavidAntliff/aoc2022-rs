40318c57307a262e65f12c4c26b06ad3f9e4f233
David Antliff <david@pitchblende.co.nz>
2023-09-03 07:27:38 +1200

    Day 12 work towards WASM.

--- bin/day12/src/lib.rs
+++ bin/day12/src/lib.rs
@@ -4,15 +4,21 @@ use svg::Document;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-impl fmt::Debug for Grid {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        for r in 0..self.0.rows() {
-            for c in 0..self.0.cols() {
-                write!(f, "{:?}", self.0[r][c])?;
-            }
-            writeln!(f)?;
+#[derive(Default, Copy, Clone)]
+enum Cell {
+    #[default]
+    Start,
+    End,
+    Height(u8),
+}
+
+impl Cell {
+    fn elevation(self) -> u8 {
+        match self {
+            Cell::Start => 0,
+            Cell::End => 25,
+            Cell::Height(e) => e,
         }
-        Ok(())
     }
 }
 
@@ -28,23 +34,52 @@ impl fmt::Debug for Cell {
     }
 }
 
-#[derive(Default)]
-enum Cell {
-    #[default]
-    Start,
-    End,
-    Height(u8),
-}
-
 #[wasm_bindgen]
 pub struct Grid(grid::Grid<Cell>);
 
+impl fmt::Debug for Grid {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for r in 0..self.0.rows() {
+            for c in 0..self.0.cols() {
+                write!(f, "{:?}", self.0[r][c])?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
 #[wasm_bindgen]
 impl Grid {
+    fn height(&self) -> usize {
+        self.0.rows()
+    }
+
+    fn width(&self) -> usize {
+        self.0.cols()
+    }
+
+    fn in_bounds(&self, coord: GridCoord) -> bool {
+        coord.x < self.0.cols() && coord.y < self.0.rows()
+    }
+
+    fn cell(&self, coord: GridCoord) -> Option<&Cell> {
+        if !self.in_bounds(coord) {
+            return None;
+        }
+        Some(&self.0[coord.y][coord.x])
+    }
+
+    fn cell_mut(&mut self, coord: GridCoord) -> Option<&mut Cell> {
+        if !self.in_bounds(coord) {
+            return None;
+        }
+        Some(&mut self.0[coord.y][coord.x])
+    }
+
     #[wasm_bindgen(constructor)]
     pub fn parse(input: &str) -> Grid {
         panic::set_hook(Box::new(console_error_panic_hook::hook));
-        console::log_1(&"Hello using web-sys".into());
 
         let lines: Vec<String> = input.lines().map(String::from).collect();
         let num_cols = lines[0].len();
@@ -71,16 +106,12 @@ impl Grid {
     pub fn to_svg(&self) -> String {
         const SIDE: usize = 64;
 
-        let mut document = Document::new().set(
-            "viewBox",
-            (0, 0, self.0.cols() * SIDE, self.0.rows() * SIDE),
-        );
+        let mut document =
+            Document::new().set("viewBox", (0, 0, self.width() * SIDE, self.height() * SIDE));
 
-        for y in 0..self.0.rows() {
-            for x in 0..self.0.cols() {
-                console::log_3(&"x ".into(), &x.into(), &y.into());
-
-                let cell = self.0.get(y, x).unwrap();
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                let cell = self.cell((x, y).into()).unwrap();
                 let (title, r, g, b) = match cell {
                     Cell::Start => ("start".to_string(), 216, 27, 96),
                     Cell::End => ("end".to_string(), 30, 136, 229),
@@ -104,7 +135,73 @@ impl Grid {
             }
         }
 
+        let defs = svg::node::element::Definitions::new().add(
+            svg::node::element::Marker::new()
+                .set("id", "arrowhead")
+                .set("markerWidth", 10)
+                .set("markerHeight", 7)
+                .set("refX", 10)
+                .set("refY", 3.5)
+                .set("orient", "auto")
+                .add(
+                    svg::node::element::Polygon::new()
+                        .set("points", "0 0, 10 3.5, 0 7")
+                        .set("fill", "#ffc107"),
+                ),
+        );
+        document = document.add(defs);
+
+        for y in 0..self.height() {
+            for x in 0..self.width() {
+                console::log_3(&"x, y ".into(), &x.into(), &y.into());
+                let coord: GridCoord = (x, y).into();
+                for ncoord in self.walkable_neighbors(coord) {
+                    let side = SIDE as f64;
+                    let (x, y) = (x as f64, y as f64);
+                    let dx = ncoord.x as f64 - x;
+                    let dy = ncoord.y as f64 - y;
+                    console::log_3(&"dx, dy ".into(), &dx.into(), &dy.into());
+                    console::log_3(
+                        &"x1, y1 ".into(),
+                        &(x + 0.5 + dx * 0.05).into(),
+                        &(y + 0.5 + dy * 0.05).into(),
+                    );
+                    console::log_3(
+                        &"x2, y2 ".into(),
+                        &(x + 0.5 + dx * 0.45).into(),
+                        &(y + 0.5 + dy * 0.45).into(),
+                    );
+
+                    let line = svg::node::element::Line::new()
+                        .set("x1", (x + 0.5 + dx * 0.05) * side)
+                        .set("y1", (y + 0.5 + dy * 0.05) * side)
+                        .set("x2", (x + 0.5 + dx * 0.45) * side)
+                        .set("y2", (y + 0.5 + dy * 0.45) * side)
+                        .set("stroke", "#ffc107")
+                        .set("stroke-width", "1px")
+                        .set("marker-end", "url(#arrowhead)");
+                    document = document.add(line);
+                }
+            }
+        }
+
         document.to_string()
+    }
+
+    fn walkable_neighbors(&self, coord: GridCoord) -> impl Iterator<Item = GridCoord> + '_ {
+        let curr_elev = self.cell(coord).unwrap().elevation();
+        let deltas: [(isize, isize); 4] = [(-1, 0), (1, 0), (0, -1), (0, 1)];
+        deltas.into_iter().filter_map(move |(dx, dy)| {
+            Some(GridCoord {
+                x: coord.x.checked_add_signed(dx)?,
+                y: coord.y.checked_add_signed(dy)?,
+            })
+            .filter(|&coord| self.in_bounds(coord))
+            .filter(|&coord| {
+                let other_elev = self.cell(coord).unwrap().elevation();
+                other_elev <= curr_elev + 1
+            })
+        })
     }
 }
 
@@ -118,8 +215,4 @@ impl From<(usize, usize)> for GridCoord {
     fn from((x, y): (usize, usize)) -> Self {
         Self { x, y }
     }
-}
-
-fn in_bounds(grid: &Grid, coord: GridCoord) -> bool {
-    coord.x < grid.0.cols() && coord.y < grid.0.rows()
 }
